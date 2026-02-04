5ad263a127c7a5efb9c21a45b2f67c64e51caf96
Fl4k228 <valovg79@gmail.com>
2026-02-04 22:30:41 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Progpam
+{
+    static int Power(int a, int n)
+    {
+        if (n == 0) 
+            return 1; 
+        return a * Power(a, n - 1);
+    }
+    static void Main()
+    {
+        Console.WriteLine(Power(2, 3));
+    }
+}
